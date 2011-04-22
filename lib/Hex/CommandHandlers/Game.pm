1dabef22acf489d1766b4c4302536b12e6a9e092
Carl Masak <cmasak@gmail.com>
2011-04-22 18:28:14 +0200

    [Hex::CommandHandlers::Game] improvements
    More like what I think we want.

--- lib/Hex/CommandHandlers/Game.pm
+++ lib/Hex/CommandHandlers/Game.pm
@@ -1,12 +1,23 @@
+package Hex::CommandHandlers::Game;
+
 use Hex::EventAggregator;
 use Hex::Command::PlaceStone;
 
-Hex::EventAggregator->subscribe(
-    Hex::Command::PlaceStone,
-    sub {
-        my $c = shift;
-        print "yay in command handler\n";
-    }
+has 'repository' => (
+    is => 'ro',
+    isa => 'Hex::Repository',
+    required => 1,
 );
+
+Hex::EventAggregator->subscribe(
+    'Hex::Command::PlaceStone' => \&handle_place_stone
+);
+
+sub handle_place_stone {
+    my ($self, $command) = @_;
+
+    my $game = $self->repository->get_by_id($c->GameID);
+    $aggregate->place_stone($game, $c->Cell);
+}
 
 1;
