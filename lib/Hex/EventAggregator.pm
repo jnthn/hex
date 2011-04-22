4f1cc5a9252451fa977c0b4aba733b07ca91b538
Jonathan Worthington <jnthn@jnthn.net>
2011-04-22 17:06:13 +0200

    Stub out a simple event aggregator.

---
+++ lib/Hex/EventAggregator.pm
@@ -0,0 +1,32 @@
+package Hex::EventAggregator;
+use Moose;
+
+# Hash of array storing subscriptions.
+my %subscriptions;
+
+# Subscribe a handler to a message.
+sub subscribe {
+    my ($self, $type, $handler) = @_;
+    $subscriptions{$type} ||= [];
+    push @{$subscriptions{$type}}, $handler;
+}
+
+# Send a message. Must have exactly one handler.
+sub send {
+    my ($self, $to_publish);
+    my $subs = $subscriptions{ref($to_publish)};
+    if (!$subs || @$subs != 1) {
+        $subs->[0]->($to_publish);
+    }
+}
+
+# Publish a message. May have zero or more subscribers.
+sub publish {
+    my ($self, $to_publish);
+    my $subs = $subscriptions{ref($to_publish)} || [];
+    for (@$subs) {
+        $_->($to_publish);
+    }
+}
+
+1;
