ab9fc45d26d1b00b7ecbd4fcee34d588fe054394
Carl Masak <cmasak@gmail.com>
2011-04-22 14:40:43 +0200

    [TestFixture] added
    This is a first approximation of what we'll eventually run our tests on.

---
+++ lib/TestFixture.pm
@@ -0,0 +1,39 @@
+package TestFixture;
+
+use Moose;
+
+has 'root' => (
+    is => 'ro',
+    isa => 'AggregateRoot',
+);
+
+has 'given' => (
+    is => 'ro',
+    isa => 'Array[Event]',
+);
+
+has 'when' => (
+    is => 'ro',
+    isa => 'CodeRef',
+);
+
+has 'then' => (
+    is => 'ro',
+    isa => 'Array[Event]|Exception',
+);
+
+sub run {
+    my ($self) = @_;
+
+    my $root = $self->root();
+    $root->load_from_history( $self->given() );
+    my @events;
+    eval {
+        $self->when()->();
+        @events = $root->get_changes();
+    }
+    # hm, handle the result here somehow
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
